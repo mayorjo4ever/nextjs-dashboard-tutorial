97182f631991161c133cf519284e4c58536a16fc
Isaac Mayowa OJO <mayorjo4ever@gmail.com>
2023-11-04 11:54:35 +0100

    NextJS Dashboard Tutorial

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Page()
+{
+    return <div><h2> Dashboard / Invoices Page </h2></div>;
+}
---
+++ app/dashboard/reports/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Page()
+{
+    return <div><h2> Dashboard / Report Page </h2></div>;
+}
